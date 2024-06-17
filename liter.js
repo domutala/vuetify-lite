302d06cfe2e22d7504f8c70686582ea9d4918839
Mamadou Talla DIA <33329431+domutala@users.noreply.github.com>
2024-06-17 00:59:34 +0000

    remove css import

--- liter.js
+++ liter.js
@@ -1,15 +1,15 @@
+const fs = require("fs");
+
 function bootstrap() {
   const cssFiles = [];
 
   function _liter(from = "") {
     const cssImportRegex = /\bimport\s*(['"])(.*?)\1\s*;/g;
 
-    const dirs = fs.readdirSync(
-      resolver.resolve(`./runtime/vuetify/lib/${from}`)
-    );
+    const dirs = fs.readdirSync(`./lib/${from}`);
 
     for (const dir of dirs) {
-      const path = resolver.resolve(`./runtime/vuetify/lib/${from}/${dir}`);
+      const path = `./lib/${from}/${dir}`;
       if (fs.statSync(path).isDirectory()) _liter(`${from}/${dir}`);
       else {
         if (dir.endsWith(".mjs")) {
@@ -37,9 +37,7 @@ function bootstrap() {
   _liter();
 
   if (cssFiles.length) {
-    const path = resolver.resolve(
-      `./runtime/vuetify/lib/styles/components.css`
-    );
+    const path = `./lib/styles/components.css`;
     fs.writeFileSync(path, cssFiles.join("\n"));
   }
 }
